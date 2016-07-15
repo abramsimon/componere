92da8d74aeaed1453a6b51d7e8fbc1962ae41cf4
Mostafa Gabriel <mgabriel@premise.com>
2016-07-15 12:09:00 -0700

    Supporting multiple input files load
    Summary:
    With this commit you can now have multiple input *.comp files having the objects
    
    Follow-ups:
    
      - To be called from the main function after landing {D5139}
    
    Test Plan: Added tests, run with ##$ python -m unittest discover##
    
    Reviewers: jim
    
    Reviewed By: jim
    
    Subscribers: moustafa, gary, code-reviews-list
    
    Differential Revision: https://phabricator.premise.com/D5146

--- componere/load_input.py
+++ componere/load_input.py
@@ -60,3 +60,34 @@ def load_areas(file):
 		return None
 
 	return Area.from_collection_dict(areas_dict)
+
+def load_file(file, areas, components, levels, teams):
+	file = open(file)
+	dict = yaml.safe_load(file)
+	file.close()
+
+	if dict is None:
+		return
+	for identifier, values_dict in dict.iteritems():
+		if identifier == "areas":
+			areas.update(Area.from_collection_dict(values_dict))
+		elif identifier == "components":
+			components.update(Component.from_collection_dict(values_dict))
+		elif identifier == "levels":
+			levels.update(Level.from_collection_dict(values_dict))
+		elif identifier == "teams":
+			teams.update(Team.from_collection_dict(values_dict))
+		else:
+			print "Undefined identifier %s" % identifier
+
+def load_objects(path, areas, components, levels, teams):
+	if os.path.isfile(path) and path.endswith(".comp"):
+		load_file(path, areas, components, levels, teams)
+	elif os.path.isdir(path):
+		for tup in os.walk(path, topdown=True):
+		    for name in tup[2]:
+				file = os.path.join(tup[0], name)
+				if file.endswith(".comp"):
+					load_file(file, areas, components, levels, teams)
+	else:
+		raise Exception("Invalid File/Directory {0}".format(path))
---
+++ tests/test_load_input.py
@@ -0,0 +1,33 @@
+from unittest import TestCase
+from componere.load_input import load_objects
+
+class LoadInputTest(TestCase):
+    def setUp(self):
+        self.areas = {}
+        self.components = {}
+        self.levels = {}
+        self.teams = {}
+
+    def test_single_file_load(self):
+        path = "../componere/tests/test_file1.comp"
+        load_objects(path, self.areas, self.components, self.levels, self.teams)
+        self.assertEqual(len(self.areas), 3)
+        self.assertEqual(len(self.components), 1)
+        self.assertEqual(len(self.levels), 2)
+        self.assertEqual(len(self.teams), 0)
+
+    def test_directory_load(self):
+        path = "../componere/tests"
+        load_objects(path, self.areas, self.components, self.levels, self.teams)
+        self.assertEqual(len(self.areas), 4)
+        self.assertEqual(len(self.components), 2)
+        self.assertEqual(len(self.levels), 2)
+        self.assertEqual(len(self.teams), 4)
+
+    def test_nested_directory_load(self):
+        path = "../componere/tests/nested_folder"
+        load_objects(path, self.areas, self.components, self.levels, self.teams)
+        self.assertEqual(len(self.areas), 0)
+        self.assertEqual(len(self.components), 0)
+        self.assertEqual(len(self.levels), 0)
+        self.assertEqual(len(self.teams), 2)
